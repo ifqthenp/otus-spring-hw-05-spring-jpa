6ccaf61a3463d3011cabe67f2dbcfa3a594ddcc7
Andrei Bogomja <bogomja@gmail.com>
2019-01-28 19:27:03 +0000

    Add jpa annotations

--- src/main/java/com/otus/hw_05/domain/Genre.java
+++ src/main/java/com/otus/hw_05/domain/Genre.java
@@ -4,12 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "genres")
 public class Genre {
 
+    @Id
+    @GeneratedValue
     private long id;
+
+    @Column(name = "genre")
     private String genre;
 
     public Genre(final String genre) {
