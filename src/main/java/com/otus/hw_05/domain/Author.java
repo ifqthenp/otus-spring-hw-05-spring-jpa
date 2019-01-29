8cf41da7e1a34c9d1e7ca5d328a9763f5efeb095
Andrei Bogomja <bogomja@gmail.com>
2019-01-29 16:32:46 +0000

    Add author entity class

--- src/main/java/com/otus/hw_05/domain/Author.java
+++ src/main/java/com/otus/hw_05/domain/Author.java
@@ -1,16 +1,29 @@
 package com.otus.hw_05.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "authors")
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @EqualsAndHashCode.Include
+    @Column(name = "first_name")
     private String firstName;
+
+    @EqualsAndHashCode.Include
+    @Column(name = "last_name")
     private String lastName;
 
     public Author(final String firstName, final String lastName) {
