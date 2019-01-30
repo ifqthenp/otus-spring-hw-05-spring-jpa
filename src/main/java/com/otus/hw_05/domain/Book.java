6cf8ea0dfa4151416bf096c3808e9c8c85f3d939
Andrei Bogomja <bogomja@gmail.com>
2019-01-30 11:45:59 +0000

    Add book entity class

--- src/main/java/com/otus/hw_05/domain/Book.java
+++ src/main/java/com/otus/hw_05/domain/Book.java
@@ -1,18 +1,33 @@
 package com.otus.hw_05.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "books")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "fk_author_id")
     private long authorId;
+
+    @Column(name = "fk_genre_id")
     private long genreId;
+
+    @Column(name = "written")
     private String year;
 
     public Book(final String title, final long authorId, final long genreId, final String year) {
